27bc0fb9a8bd9e65e7b41e8a6d3eb1db2a75f634
M S Shankar <s.sathyakumari@kigroup.de>
2019-10-21 14:27:12 +0200

    k-20: Rebased

--- cli/kaos_cli/commands/build.py
+++ cli/kaos_cli/commands/build.py
@@ -1,8 +1,9 @@
 import os
 import sys
-import time
 
 import click
+from kaos_cli.constants import AWS, GCP, DOCKER, MINIKUBE
+from kaos_cli.utils.validators import validate_build_env
 from kaos_cli.exceptions.handle_exceptions import handle_specific_exception, handle_exception
 from kaos_cli.facades.backend_facade import BackendFacade, is_cloud_provider
 from kaos_cli.utils.decorators import build_env_check, pass_obj
@@ -17,21 +18,21 @@ from kaos_cli.utils.decorators import in_dir
                short_help='{}'.format(
                    click.style('Build the kaos backend', bold=True, fg='black')))
 @click.option('-c', '--cloud', type=click.Choice([DOCKER, MINIKUBE, AWS, GCP]),
-              help='selected provider provider', required=True)
-@click.option('-e', '--env', type=click.Choice(['prod', 'stage', 'dev']), default='prod',
-              help='selected infrastructure environment', required=False)
+              help='selected provider', required=True)
+@click.option('-e', '--env', type=click.Choice(['prod', 'stage', 'dev']),
+              help='selected environment [cloud only]', required=False)
 @click.option('-f', '--force', is_flag=True,
-              help='force building infrastructure', required=False)
+              help='force build', required=False)
 @click.option('-v', '--verbose', is_flag=True,
               help='verbose output', required=False)
 @click.option('-y', '--yes', is_flag=True,
               help='answer yes to any prompt', required=False)
 @click.option('-l', '--local_backend', is_flag=True,
-              help='terraform will store backend locally, only relevant for AWS and GCP', required=False)
+              help='locally store terraform state [cloud only]', required=False)
 @build_env_check
 def build(cloud, env, force, verbose, yes, local_backend):
     """
-    Deploy kaos backend infrastructure based on selected provider.
+    Deploy kaos backend infrastructure based on the selected provider.
     """
 
     # Computing kaos state directory based on cloud and env before destroy
@@ -52,38 +53,69 @@ def build(cloud, env, force, verbose, yes, local_backend):
             sys.exit(1)
 
         elif is_created and force:
-            click.echo('Performing force build of the backend')
+            click.echo('{} - Performing {} build of the backend'.format(
+                click.style("Warning", bold=True, fg='yellow'),
+                click.style("force", bold=True)))
+
+        # validate ENV
+        env = validate_build_env(cloud, env)
 
         if not yes:
             # confirm creation of backend
-            click.confirm(
-                'Are you sure about building {} [{}] backend in {}?'.format(click.style('kaos', bold=True, fg='green'),
-                                                                            click.style(env, bold=True, fg='blue'),
-                                                                            click.style(cloud, bold=True, fg='red')),
-                abort=True)
+            if env:
+                click.confirm(
+                    '{} - Are you sure about building {} [{}] backend in {}?'.format(
+                        click.style("Warning", bold=True, fg='yellow'),
+                        click.style('kaos', bold=True),
+                        click.style(env, bold=True, fg='blue'),
+                        click.style(cloud, bold=True, fg='red')),
+                    abort=True)
+            else:
+                click.confirm(
+                    '{} - Are you sure about building {} backend in {}?'.format(
+                        click.style("Warning", bold=True, fg='yellow'),
+                        click.style('kaos', bold=True),
+                        click.style(cloud, bold=True, fg='red')),
+                    abort=True)
 
         if local_backend:
-            click.echo('Building with local backend! The terraform state exists only on this local machine!')
+            click.echo('{} - Building with {} terraform backend state'.format(
+                click.style("Info", bold=True, fg='green'),
+                click.style("local", bold=True)))
 
         if local_backend and cloud in [DOCKER, MINIKUBE]:
-            click.echo(f'-l/--local_backend flag has been specified but cloud type is {cloud}, '
-                       f'the flag has no effect as the backend is always local in this case.')
+            click.echo('{} - local backend (-l/--local_backend) has no effect for {}'.format(
+                click.style("Info", bold=True, fg='green'),
+                click.style(cloud, bold=True, fg='red')))
 
         try:
 
             backend.build(cloud, env, dir_build, local_backend=local_backend, verbose=verbose)
 
             if verbose:
-                click.echo("Endpoint successfully set to {}".format(click.style(backend.url, bold=True, fg='green')))
+                click.echo("\n{} - Endpoint successfully set to {}".format(
+                    click.style("Info", bold=True, fg='green'),
+                    click.style(backend.url, bold=True, fg='green')))
 
             if is_cloud_provider(cloud):
                 kubeconfig = os.path.abspath(backend.kubeconfig)
-                click.echo("To interact with the Kubernetes cluster:\n\n {}"
-                           .format(click.style("export KUBECONFIG=" + kubeconfig,
+                click.echo("\n{} - To interact with the Kubernetes cluster:\n {}"
+                           .format(click.style("Info", bold=True, fg='green'),
+                                   click.style("export KUBECONFIG=" + kubeconfig,
                                                bold=True, fg='red')))
 
             click.echo("Successfully built {} [{}] environment".format(click.style('kaos', bold=True, fg='green'),
                                                                        click.style(env, bold=True, fg='blue')))
+
+            if env:
+                click.echo("{} - Successfully built {} [{}] environment".format(
+                    click.style("Info", bold=True, fg='green'),
+                    click.style('kaos', bold=True),
+                    click.style(env, bold=True, fg='blue')))
+            else:
+                click.echo("{} - Successfully built {} environment".format(
+                    click.style("Info", bold=True, fg='green'),
+                    click.style('kaos', bold=True)))
 
         except Exception as e:
             handle_specific_exception(e)
@@ -96,9 +128,9 @@ def build(cloud, env, force, verbose, yes, local_backend):
                short_help='{}'.format(
                    click.style('Destroy the kaos backend', bold=True, fg='black')))
 @click.option('-c', '--cloud', type=click.Choice([DOCKER, MINIKUBE, AWS, GCP]),
-              help='selected provider provider', required=True)
-@click.option('-e', '--env', type=click.Choice(['prod', 'stage', 'dev']), default='prod',
-              help='selected infrastructure environment', required=True)
+              help='selected provider', required=True)
+@click.option('-e', '--env', type=click.Choice(['prod', 'stage', 'dev']),
+              help='selected infrastructure environment', required=False)
 @click.option('-v', '--verbose', is_flag=True,
               help='verbose output', required=False)
 @click.option('-y', '--yes', is_flag=True,
@@ -113,24 +145,41 @@ def destroy(cloud, env, verbose, yes):
 
     dir_build = os.path.join(KAOS_STATE_DIR, build_path)
 
+    # validate ENV
+    env = validate_build_env(cloud, env)
+
     @in_dir(dir_build)
     @pass_obj(BackendFacade)
     def __destroy_backend(backend: BackendFacade, cloud, env, dir_build, verbose, yes):
         if not yes:
             # confirm creation of backend
-            click.confirm(
-                'Are you sure about destroying {} [{}] backend in {}?'.format(
-                    click.style('kaos', bold=True, fg='green'),
+            if env:
+                click.confirm('{} - Are you sure about destroying {} [{}] backend in {}?'.format(
+                    click.style("Warning", bold=True, fg='yellow'),
+                    click.style('kaos', bold=True),
                     click.style(env, bold=True, fg='blue'),
                     click.style(cloud, bold=True, fg='red')),
-                abort=True)
-
+                    abort=True)
+            else:
+                click.confirm(
+                    '{} - Are you sure about destroying {} backend in {}?'.format(
+                        click.style("Warning", bold=True, fg='yellow'),
+                        click.style('kaos', bold=True),
+                        click.style(cloud, bold=True, fg='red')),
+                    abort=True)
         try:
 
             backend.destroy(cloud, env, dir_build, verbose=verbose)
 
-            click.echo("Successfully destroyed {} [{}] environment".format(click.style('kaos', bold=True, fg='green'),
-                                                                           click.style(env, bold=True, fg='blue')))
+            if env:
+                click.echo(
+                    "{} - Successfully destroyed {} [{}] environment".format(click.style("Info", bold=True, fg='green'),
+                                                                             click.style('kaos', bold=True),
+                                                                             click.style(env, bold=True, fg='blue')))
+            else:
+                click.echo(
+                    "{} - Successfully destroyed {} environment".format(click.style("Info", bold=True, fg='green'),
+                                                                        click.style('kaos', bold=True)))
 
         except Exception as e:
             handle_specific_exception(e)
