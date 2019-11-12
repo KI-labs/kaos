954cd562e3c09c22dc37631cf6ba281beed6605e
w.jurasz <w.jurasz@kigroup.de>
2019-11-12 15:15:30 +0100

    Added Empty to None mapping

--- backend/kaos_backend/util/flask.py
+++ backend/kaos_backend/util/flask.py
@@ -1,5 +1,6 @@
 import functools
 from flask import jsonify as flask_jsonify
+from google.protobuf.empty_pb2 import Empty
 
 from kaos_model.api import Response, PagedResponse, Error
 
@@ -11,5 +12,8 @@ def jsonify(f):
         obj = f(*args, **kwargs)
         if type(obj) in (Response, PagedResponse, Error):
             obj = obj.to_dict()
+        if type(obj) is dict:
+            obj = {k: None if isinstance(v, Empty) else v for k, v in obj.items()}
         return flask_jsonify(obj)
+
     return wrapped
